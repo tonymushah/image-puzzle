d408f2f6c457d40a9e1a5e813e6ba53576b51dbb
tonymushah <tonyramanantsalama@gmail.com>
2024-02-07 18:22:44 +0300

    Added mathjs

--- src-tauri/puzzle-builder/examples/game_save_new.rs
+++ src-tauri/puzzle-builder/examples/game_save_new.rs
@@ -2,8 +2,13 @@ use anyhow::Result;
 use puzzle_builder::game::{dim::GameDimension, save::GameSave};
 
 fn main() -> Result<()> {
-    let save = GameSave::new("./test-assets/marguerite-729510_640.jpg", "./games/test-3", GameDimension::new_rec(3, 4))?;
+    let save = GameSave::new(
+        "./test-assets/marguerite-729510_640.jpg",
+        "./games/test-3",
+        GameDimension::new_rec(3, 4),
+    )?;
     let load = GameSave::load("./games/test-3")?;
+    println!("{:#?}", load);
     assert_eq!(save, load);
     Ok(())
 }
--- src-tauri/puzzle-builder/src/game/save.rs
+++ src-tauri/puzzle-builder/src/game/save.rs
@@ -60,7 +60,7 @@ impl GameSave {
         let image = open(&image_path)?;
         let images = split_image(image, size)?;
         let images_path =
-            images.map_with_location(|_, _, _| format!("{}.{}", Uuid::new_v4(), extension));
+            images.map_with_location(|y, x, _| format!("{x}-{y}-{}.{}", Uuid::new_v4(), extension));
         for (y, row) in images.row_iter().enumerate() {
             for (x, colums) in row.column_iter().enumerate() {
                 println!("{x}-{y}");
