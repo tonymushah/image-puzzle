542043eed8cbfce4496e892924d1638262869eea
tonymushah <tonyramanantsalama@gmail.com>
2024-02-07 17:52:32 +0300

    Added plugin in the tauri main

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(puzzle_builder::get_plugin())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
